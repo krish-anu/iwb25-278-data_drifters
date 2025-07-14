d21d391b403505fe53ab3449786c75ff3151ccd5
krish-anu <krishnaanu200302@gmail.com>
2025-07-14 22:07:04 +0530

    Project initialized

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import {Button} from "./components/ui/button"
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Project Started</h1>
+      <Button variant="destructive">Click me</Button>
+    </>
+  );
+}
+
+export default App
